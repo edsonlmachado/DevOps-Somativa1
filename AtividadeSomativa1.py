f56563dc2322bd2ca1e5c3e577d9156d6db3a690
edsonlmachado <luizmachado.edson@gmail.com>
2024-03-29 23:06:45 -0300

    publicar projeto AtividadeSomativa1.py

---
+++ AtividadeSomativa1.py
@@ -0,0 +1,19 @@
+import time
+
+def contar_piada_devops():
+    print("Aqui vai uma piada de DevOps:")
+    time.sleep(2)
+
+    print("Por que o DevOps sempre leva um guarda-chuva para o trabalho?")
+    time.sleep(3)
+    
+    
+    print("Porque ele sempre quer estar preparado para o fluxo contínuo!")
+    time.sleep(2)
+
+    print("Prof.-Tutor: Fellipe Medeiros Veiga riu que eu vi :)")
+    time.sleep(2)
+
+# Chamar a função para contar a piada
+contar_piada_devops()
+
